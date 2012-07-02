be0c7fcda7798cf51088f4f1d21f7c9840cf024b
Amos Kong <akong@redhat.com>
2012-07-02 11:05:49 +0800

    improve echo sentences to edit config file
    Signed-off-by: Amos Kong <akong@redhat.com>

--- scripts/install-numpy-scipy.sh
+++ scripts/install-numpy-scipy.sh
@@ -11,8 +11,7 @@ wget http://www.netlib.org/lapack/lapack-3.3.0.tgz
 tar xzvf lapack-3.3.0.tgz
 cd lapack-3.3.0
 cp INSTALL/make.inc.gfortran make.inc
-echo "----------> vim make.inc"
-echo "add -fPIC to OPTS and NOOPT"
+echo "[debug] edit make.inc, add -fPIC to OPTS and NOOPT"
 echo "OPTS     = -O2 -fPIC" >> make.inc
 echo "NOOPT     = -O0 -fPIC" >> make.inc
 
@@ -29,7 +28,7 @@ mkdir ATLAS_LINUX ; cd ATLAS_LINUX
 cpufreq-selector -g performance
 ../configure -Fa alg -fPIC -Si cputhrchk 0 --prefix=/usr/local/atlas-3.9.35 --with-netlib-lapack-tarfile=/usr/local/src/lapack-3.3.0.tgz 2>&1 | tee log.config
 make 2>&1 | tee log.make
-echo "THIS WILL TAKE HOURS TO MAKE!!!!!!"
+echo "[debug] THIS WILL TAKE HOURS TO MAKE!!!!!!"
 make install 2>&1 | tee log.install
 cd /usr/local ; ln -s atlas-3.9.35 atlas
 export LD_LIBRARY_PATH=/usr/local/atlas/lib:$LD_LIBRARY_PATH
@@ -39,12 +38,9 @@ wget http://cdnetworks-us-2.dl.sourceforge.net/project/numpy/NumPy/1.5.1/numpy-1
 tar xzvf numpy-1.5.1.tar.gz
 cd numpy-1.5.1
 cp site.cfg.example site.cfg
-echo "----------> vim site.cfg"
-echo "[DEFAULT]" >> site.cfg
-echo "library_dirs = /usr/local/atlas/lib" >> site.cfg
-echo "include_dirs = /usr/local/atlas/include" >> site.cfg
-echo "[atlas]" >> site.cfg
-echo "atlas_libs = lapack, f77blas, cblas, atlas" >> site.cfg
+echo "[debug] edit site.cfg"
+echo -e "[DEFAULT]\nlibrary_dirs = /usr/local/atlas/lib\ninclude_dirs = /usr/local/atlas/include" >> site.cfg
+echo -e "[atlas]\natlas_libs = lapack, f77blas, cblas, atlas" >> site.cfg
 
 python setup.py build 2>&1 | tee log.build
 python setup.py install --prefix=/usr/local/numpy-1.5.1 2>&1 | tee log.install
@@ -61,6 +57,6 @@ python setup.py build 2>&1 | tee log.build
 python setup.py install --prefix=/usr/local/scipy-0.9.0rc3 2>&1 | tee log.install
 cd /usr/local ; ln -s scipy-0.9.0rc3 scipy
 
-echo $?
+echo "Exec status: $?"
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/numpy/lib64/python2.6/site-packages:/usr/local/scipy/lib64/python2.6/site-packages
